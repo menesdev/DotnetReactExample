c3c89aa1dc45bd2e571b4507607f21b847ee6583
Mustafa Enes Güneruz <mustafaguneruz19@gmail.com>
2021-10-31 11:23:26 +0300

    Initial commit

---
+++ ClientApp/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Header = () => {
+
+    const headerStyle = {
+        width: '100%',
+        padding: '2%',
+        backgroundColor: 'yellowgreen',
+        color: 'black',
+        textAlign: 'center'
+
+    }
+
+    return (
+        <div style={headerStyle}>
+            <h1>React with .NET</h1>
+        </div>
+    )
+
+
+}
